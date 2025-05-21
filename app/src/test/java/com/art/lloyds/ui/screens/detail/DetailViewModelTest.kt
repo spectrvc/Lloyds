e2cad9797a4639848256563b273beaca25f19f62
Artem Budnyk <spectrvc@gmail.com>
2025-05-21 12:44:15 +0100

    Refactoring unit tests for viewModels. Created a unified structure for unit tests to make the tests themselves more readable and concise

--- app/src/test/java/com/art/lloyds/ui/screens/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/art/lloyds/ui/screens/detail/DetailViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.art.lloyds.ui.screens.detail
 
 import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
 import com.art.domain.common.DispatcherProvider
 import com.art.domain.common.GlobalConst.Companion.NAVIGATION_OPTIONS
 import com.art.domain.common.GlobalConst.Companion.NAVIGATION_OPTIONS_ERROR
@@ -13,10 +14,9 @@ import com.google.gson.Gson
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
@@ -25,29 +25,13 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
 class DetailViewModelTest {
 
-    private lateinit var viewModel: DetailViewModel
     private val navigationUseCase = mock<NavigationUseCase>()
     private val storeUseCase = mock<StoreUseCase>()
-
-    //Random test data
-    private val apiResult = ApiResult.Success(
-        ProductDto(
-            id = 2194,
-            title = "125",
-            category = "325",
-            price = 2.3,
-            image = "451"
-        )
-    )
-    private val productId = 154
-    private val options = ScreenOptions.DetailOptions(productId = productId)
-    private val savedStateHandle = SavedStateHandle().apply {
-        set(NAVIGATION_OPTIONS, Gson().toJson(options))
-    }
 
     @AfterEach
     fun afterEach() {
@@ -55,120 +39,173 @@ class DetailViewModelTest {
         Mockito.reset(storeUseCase)
     }
 
-    private fun createViewModel(
-        testScheduler: TestCoroutineScheduler,
-        savedStateHandle: SavedStateHandle,
-    ) {
-        val testDispatcher = StandardTestDispatcher(testScheduler)
-        val dispatcherProvider = DispatcherProvider(
-            io = testDispatcher,
-            main = testDispatcher,
-            default = testDispatcher
-        )
-        viewModel = DetailViewModel(
-            dispatcherProvider = dispatcherProvider,
-            savedStateHandle = savedStateHandle,
-            navigationUseCase = navigationUseCase,
-            storeUseCase = storeUseCase,
-        )
+    /////////////////////////////////////////// TEST DATA
+
+    private val productId = 154
+    private val options = ScreenOptions.DetailOptions(productId = productId)
+    private val apiResult = ApiResult.Success(ProductDto(id = productId))
+    private val savedStateHandle = SavedStateHandle().apply {
+        set(NAVIGATION_OPTIONS, Gson().toJson(options))
+    }
+
+    /////////////////////////////////////////// BUILDER
+
+    private inner class ViewModelBuilder {
+
+        fun build(
+            testDispatcher: TestDispatcher,
+            savedStateHandle: SavedStateHandle,
+        ): DetailViewModel {
+            val dispatcherProvider = DispatcherProvider(
+                io = testDispatcher,
+                main = testDispatcher,
+                default = testDispatcher
+            )
+            return DetailViewModel(
+                dispatcherProvider = dispatcherProvider,
+                savedStateHandle = savedStateHandle,
+                navigationUseCase = navigationUseCase,
+                storeUseCase = storeUseCase
+            )
+        }
+
+        suspend fun withGetProductResult(): ViewModelBuilder {
+            Mockito.`when`(storeUseCase.getProduct(any())).doReturn(apiResult)
+            return this
+        }
+
+    }
+
+    /////////////////////////////////////////// CASES
+
+    internal inner class Cases(private val viewModel: DetailViewModel) {
+
+        fun userOnClickBack(): Cases {
+            viewModel.setEvent(DetailEvent.OnClickBack)
+            return this
+        }
+
+    }
+
+    /////////////////////////////////////////// VERIFIER
+
+    private fun assertThat(viewModel: DetailViewModel): ViewModelVerifier {
+        return ViewModelVerifier(viewModel)
+    }
+
+    private inner class ViewModelVerifier(private val viewModel: DetailViewModel) {
+
+        suspend fun indicatorWasShownThenHidden(): ViewModelVerifier {
+            viewModel.uiState.map { it.indicatorVisibility }
+                .distinctUntilChanged()
+                .test {
+                    assertEquals(false, awaitItem())    //initial value
+                    assertEquals(true, awaitItem())     //showIndicator
+                    assertEquals(false, awaitItem())    //hideIndicator
+                    expectNoEvents()
+                }
+            return this
+        }
+
+        suspend fun navigationPopBackWasCalled(): ViewModelVerifier {
+            Mockito.verify(navigationUseCase, Mockito.times(1))
+                .popBack()
+            return this
+        }
+
+        suspend fun getProductWasCalled(): ViewModelVerifier {
+            Mockito.verify(storeUseCase, Mockito.times(1))
+                .getProduct(productId)
+            return this
+        }
+
+        fun optionsWasGot(): ViewModelVerifier {
+            assertEquals(options, viewModel.uiState.value.options)
+            return this
+        }
+
+        fun apiResultWasGot(): ViewModelVerifier {
+            assertEquals(apiResult, viewModel.uiState.value.apiResult)
+            return this
+        }
+
+    }
+
+    /////////////////////////////////////////// TESTS
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `(init) should cause (optionsWasGot)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withGetProductResult()
+            .build(testDispatcher, savedStateHandle)
+        assertThat(viewModel)
+            .optionsWasGot()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `init should set options to state`() = runTest {
-        Mockito.`when`(storeUseCase.getProduct(any())).thenReturn(apiResult)
-        createViewModel(
-            testScheduler = testScheduler,
-            savedStateHandle = savedStateHandle
-        )
-        advanceUntilIdle()
-        assertEquals(options, viewModel.uiState.value.options)
+    fun `(init) should cause (getProductWasCalled)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withGetProductResult()
+            .build(testDispatcher, savedStateHandle)
+        assertThat(viewModel)
+            .getProductWasCalled()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `init should call getProduct`() = runTest {
-        Mockito.`when`(storeUseCase.getProduct(any())).thenReturn(apiResult)
-        createViewModel(
-            testScheduler = testScheduler,
-            savedStateHandle = savedStateHandle
-        )
-        advanceUntilIdle()
-        Mockito.verify(storeUseCase, Mockito.times(1))
-            .getProduct(productId)
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun `init should throw exceptions if savedStateHandle do not contain NAVIGATION_OPTIONS`() =
+    fun `(init) and (savedStateHandle do not contain NAVIGATION_OPTIONS) should cause (throw exceptions)`() =
         runTest {
             var exception: Exception? = null
             try {
-                createViewModel(
-                    testScheduler = testScheduler,
-                    savedStateHandle = SavedStateHandle()
-                )
+                val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+                ViewModelBuilder().build(testDispatcher, SavedStateHandle())
             } catch (e: Exception) {
                 exception = e
             }
-            advanceUntilIdle()
             assertEquals(NAVIGATION_OPTIONS_ERROR, exception?.message)
-
         }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `init should set apiResult to state`() = runTest {
-        Mockito.`when`(storeUseCase.getProduct(any())).thenReturn(apiResult)
-        createViewModel(
-            testScheduler = testScheduler,
-            savedStateHandle = savedStateHandle
-        )
-        advanceUntilIdle()
-
-        assertEquals(apiResult, viewModel.uiState.value.apiResult)
+    fun `(init) should cause (apiResultWasGot)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withGetProductResult()
+            .build(testDispatcher, savedStateHandle)
+        assertThat(viewModel)
+            .apiResultWasGot()
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `init should set indicatorVisibility true then false`() =
+    fun `(init) should cause (indicatorWasShownThenHidden)`() =
         runTest {
-            Mockito.`when`(storeUseCase.getProduct(any())).thenReturn(apiResult)
-            createViewModel(
-                testScheduler = testScheduler,
-                savedStateHandle = savedStateHandle
-            )
-
-            val flowList = mutableListOf<Boolean>()
-            val job = launch(UnconfinedTestDispatcher(testScheduler)) {
-                viewModel.uiState.map { it.indicatorVisibility }
-                    .distinctUntilChanged()
-                    .toList(flowList)
+            val testDispatcher = StandardTestDispatcher(testScheduler)
+            val viewModel = ViewModelBuilder()
+                .withGetProductResult()
+                .build(testDispatcher, savedStateHandle)
+            val job = launch(testDispatcher) {
+                assertThat(viewModel)
+                    .indicatorWasShownThenHidden()
             }
             advanceUntilIdle()
-
-            assertEquals(3, flowList.size)
-            if (flowList.size == 3) {
-                assertEquals(false, flowList[0])    //initial value
-                assertEquals(true, flowList[1])     //showIndicator
-                assertEquals(false, flowList[2])    //hideIndicator
-            }
             job.cancel()
         }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `setEvent OnClickBack should navigate back`() = runTest {
-        Mockito.`when`(storeUseCase.getProduct(any())).thenReturn(apiResult)
-        createViewModel(
-            testScheduler = testScheduler,
-            savedStateHandle = savedStateHandle
-        )
-
-        viewModel.setEvent(DetailEvent.OnClickBack)
-        advanceUntilIdle()
-
-        Mockito.verify(navigationUseCase, Mockito.times(1))
-            .popBack()
+    fun `setEvent(DetailEvent) should cause (navigationPopBackWasCalled)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withGetProductResult()
+            .build(testDispatcher, savedStateHandle)
+        Cases(viewModel)
+            .userOnClickBack()
+        assertThat(viewModel)
+            .navigationPopBackWasCalled()
     }
 }
--- app/src/test/java/com/art/lloyds/ui/screens/main/MainViewModelTest.kt
+++ app/src/test/java/com/art/lloyds/ui/screens/main/MainViewModelTest.kt
@@ -1,30 +1,47 @@
 package com.art.lloyds.ui.screens.main
 
+import app.cash.turbine.test
 import com.art.domain.common.DispatcherProvider
+import com.art.domain.navigation.NavigationDto
+import com.art.domain.navigation.NavigationEnum
 import com.art.domain.navigation.NavigationUseCase
 import com.art.domain.navigation.ScreenOptions
 import com.art.domain.navigation.ScreensEnum
 import com.art.lloyds.R
+import com.art.lloyds.ui.main.baseViewModel.UiEffect
 import com.art.lloyds.ui.main.scaffold.DrawerItemData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-
+import org.mockito.kotlin.whenever
 
 class MainViewModelTest {
 
-    private lateinit var viewModel: MainViewModel
     private val navigationUseCase = mock<NavigationUseCase>()
 
-    //Random test data
+    @AfterEach
+    fun afterEach() {
+        Mockito.reset(navigationUseCase)
+    }
+
+    /////////////////////////////////////////// TEST DATA
+
+    private val drawerItemData = DrawerItemData(
+        id = ScreensEnum.ABOUT,
+        resId = 2590,
+        imageId = 2955
+    )
     private val drawerItemList = listOf(
         DrawerItemData(
             id = ScreensEnum.HOME,
@@ -37,50 +54,117 @@ class MainViewModelTest {
             imageId = R.drawable.about
         )
     )
-    private val drawerItemData = DrawerItemData(
-        id = ScreensEnum.ABOUT,
-        resId = 2590,
-        imageId = 2955
-    )
+    private val navigationDto = NavigationDto(NavigationEnum.POP_BACK)
+    val sharedFlow = MutableSharedFlow<NavigationDto>()
 
-    @AfterEach
-    fun afterEach() {
-        Mockito.reset(navigationUseCase)
+    /////////////////////////////////////////// BUILDER
+
+    private inner class ViewModelBuilder {
+
+        fun build(testDispatcher: TestDispatcher): MainViewModel {
+            val dispatcherProvider = DispatcherProvider(
+                io = testDispatcher,
+                main = testDispatcher,
+                default = testDispatcher
+            )
+            return MainViewModel(
+                dispatcherProvider = dispatcherProvider,
+                navigationUseCase = navigationUseCase,
+            )
+        }
+
+        fun withNavigationSideEffect(): ViewModelBuilder {
+            whenever(navigationUseCase.navigationSideEffect).doReturn(sharedFlow)
+            return this
+        }
+
     }
 
-    private fun createViewModel(testScheduler: TestCoroutineScheduler) {
+    /////////////////////////////////////////// CASES
+
+    internal inner class Cases(private val viewModel: MainViewModel) {
+
+        fun userNavigate(): Cases {
+            viewModel.setEvent(MainEvent.Navigate(drawerItemData))
+            return this
+        }
+
+        suspend fun navigationSideEffectWasReceived(): Cases {
+            sharedFlow.emit(navigationDto)
+            return this
+        }
+
+    }
+
+    /////////////////////////////////////////// VERIFIER
+
+    private fun assertThat(viewModel: MainViewModel): ViewModelVerifier {
+        return ViewModelVerifier(viewModel)
+    }
+
+    private inner class ViewModelVerifier(private val viewModel: MainViewModel) {
+
+        suspend fun navigateWasCalled(): ViewModelVerifier {
+            Mockito.verify(navigationUseCase, Mockito.times(1))
+                .navigate(screen = drawerItemData.id, options = ScreenOptions.EmptyOptions)
+            return this
+        }
+
+        fun drawerItemListWasGot(): ViewModelVerifier {
+            assertEquals(drawerItemList, viewModel.uiState.value.drawerItemList)
+            return this
+        }
+
+        fun uiEffectWasEmitted(uiEffect: UiEffect): ViewModelVerifier {
+            assertEquals(MainEffect.ControllerNavigate(navigationDto), uiEffect)
+            return this
+        }
+    }
+
+    /////////////////////////////////////////// TESTS
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `(init) should cause (drawerItemListWasGot)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withNavigationSideEffect()
+            .build(testDispatcher)
+        assertThat(viewModel)
+            .drawerItemListWasGot()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `setEvent(Navigate) should cause (navigateWasCalled)`() = runTest {
+        val testDispatcher = UnconfinedTestDispatcher(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withNavigationSideEffect()
+            .build(testDispatcher)
+        Cases(viewModel)
+            .userNavigate()
+        assertThat(viewModel)
+            .navigateWasCalled()
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `(navigationSideEffectWasReceived) should cause (uiEffectWasEmitted)`() = runTest {
         val testDispatcher = StandardTestDispatcher(testScheduler)
-        val dispatcherProvider = DispatcherProvider(
-            io = testDispatcher,
-            main = testDispatcher,
-            default = testDispatcher
-        )
-        viewModel = MainViewModel(
-            dispatcherProvider = dispatcherProvider,
-            navigationUseCase = navigationUseCase,
-        )
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun `init should set drawerItemList to state`() = runTest {
-        Mockito.`when`(navigationUseCase.navigationSideEffect).thenReturn(MutableSharedFlow())
-        createViewModel(testScheduler)
+        val viewModel = ViewModelBuilder()
+            .withNavigationSideEffect()
+            .build(testDispatcher)
+        val job = launch(testDispatcher) {
+            viewModel.uiEffect.test {
+                Cases(viewModel)
+                    .navigationSideEffectWasReceived()
+                assertThat(viewModel)
+                    .uiEffectWasEmitted(awaitItem())
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
         advanceUntilIdle()
-        assertEquals(drawerItemList, viewModel.uiState.value.drawerItemList)
-    }
-
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun `setEvent Navigate should call navigate`() = runTest {
-        createViewModel(testScheduler)
-        Mockito.`when`(navigationUseCase.navigationSideEffect).thenReturn(MutableSharedFlow())
-
-        viewModel.setEvent(MainEvent.Navigate(drawerItemData))
-        advanceUntilIdle()
-
-        Mockito.verify(navigationUseCase, Mockito.times(1))
-            .navigate(screen = drawerItemData.id, options = ScreenOptions.EmptyOptions)
+        job.cancel()
     }
 
 }
